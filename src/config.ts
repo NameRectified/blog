0115cae22ff7a84a7e75e56b187a8acd6c198bc4
NameRectified <85291879+NameRectified@users.noreply.github.com>
2023-12-24 19:08:48 +0530

    Change Name and Bio,

--- src/config.ts
+++ src/config.ts
@@ -1,11 +1,11 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Captain Awesome";
+export const SITE_TITLE = "Master Of Universe";
 export const SITE_DESCRIPTION =
-  "Welcome to my blog! I write about modern web development.";
-export const TWITTER_HANDLE = "@yourtwitterhandle";
-export const MY_NAME = "Captain Awesome";
+  "This is the place where I will share my journey. It's more of a journal than a blog";
+// export const TWITTER_HANDLE = "@yourtwitterhandle";
+export const MY_NAME = "Master Of Universe";
 
 // setup in astro.config.mjs
 const BASE_URL = new URL(import.meta.env.SITE);
